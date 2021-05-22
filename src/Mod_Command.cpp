f8299c10c14d63c54dda57a38c8969f980642b14
Sean Hilton <68968076+Sean-Hilton@users.noreply.github.com>
2021-05-21 22:00:10 -0400

    Update and rename Mod_Command.cpp to src/Mod_Command.cpp

--- src/Mod_Command.cpp
+++ src/Mod_Command.cpp
@@ -6,9 +6,7 @@
 
 	 int Mod_Command::evaluate(int num1, int num2)const
 	 {
-		 //COMMENT: Your code will break on mod by 0.
-
-		 //RESPONSE: added an exception if user tries to mod by 0.
+		 
 		 bool exception_caught = true;
 		 try {
 			 if (num2 == 0)
